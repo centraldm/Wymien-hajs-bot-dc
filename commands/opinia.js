2794b874fa6c31d7791e0d1ba27e537ee43d87dc
centraldm <michaltylec09@gmail.com>
2025-07-30 10:57:30 +0200

    Update opinia.js

--- commands/opinia.js
+++ commands/opinia.js
@@ -6,20 +6,45 @@ module.exports = {
     .setDescription('Wystaw opinię o wymianie.')
     .addStringOption(option =>
       option.setName('czas_realizacji')
-        .setDescription('Ocena czasu realizacji (np. ⭐⭐⭐⭐⭐)')
-        .setRequired(true))
+        .setDescription('Ocena czasu realizacji')
+        .setRequired(true)
+        .addChoices(
+          { name: '⭐️', value: '1' },
+          { name: '⭐️⭐️', value: '2' },
+          { name: '⭐️⭐️⭐️', value: '3' },
+          { name: '⭐️⭐️⭐️⭐️', value: '4' },
+          { name: '⭐️⭐️⭐️⭐️⭐️', value: '5' },
+        )
+    )
     .addStringOption(option =>
       option.setName('przebieg_wymiany')
-        .setDescription('Ocena przebiegu wymiany (np. ⭐⭐⭐⭐⭐)')
-        .setRequired(true))
+        .setDescription('Ocena przebiegu wymiany')
+        .setRequired(true)
+        .addChoices(
+          { name: '⭐️', value: '1' },
+          { name: '⭐️⭐️', value: '2' },
+          { name: '⭐️⭐️⭐️', value: '3' },
+          { name: '⭐️⭐️⭐️⭐️', value: '4' },
+          { name: '⭐️⭐️⭐️⭐️⭐️', value: '5' },
+        )
+    )
     .addStringOption(option =>
       option.setName('obsluga_klienta')
-        .setDescription('Ocena obsługi klienta (np. ⭐⭐⭐⭐⭐)')
-        .setRequired(true))
+        .setDescription('Ocena obsługi klienta')
+        .setRequired(true)
+        .addChoices(
+          { name: '⭐️', value: '1' },
+          { name: '⭐️⭐️', value: '2' },
+          { name: '⭐️⭐️⭐️', value: '3' },
+          { name: '⭐️⭐️⭐️⭐️', value: '4' },
+          { name: '⭐️⭐️⭐️⭐️⭐️', value: '5' },
+        )
+    )
     .addStringOption(option =>
       option.setName('opinia')
         .setDescription('Twoja opinia tekstowa')
-        .setRequired(true)),
+        .setRequired(true)
+    ),
 
   async execute(interaction) {
     const czas = interaction.options.getString('czas_realizacji');
@@ -28,16 +53,16 @@ module.exports = {
     const opinia = interaction.options.getString('opinia');
 
     const embed = new EmbedBuilder()
-      .setColor('#ff0000') // czerwony kolor
+      .setColor('#ff0000')
       .setAuthor({
         name: '⭐ WYMIEŃ HAJS × OPINIA',
         iconURL: interaction.user.displayAvatarURL({ dynamic: true })
       })
       .setDescription(`\`\`\`${opinia}\`\`\``)
       .addFields(
-        { name: 'CZAS REALIZACJI:', value: `${czas}`, inline: false },
-        { name: 'PRZEBIEG WYMIANY:', value: `${przebieg}`, inline: false },
-        { name: 'OBSŁUGA KLIENTA:', value: `${obsluga}`, inline: false },
+        { name: 'CZAS REALIZACJI:', value: '⭐️'.repeat(Number(czas)), inline: false },
+        { name: 'PRZEBIEG WYMIANY:', value: '⭐️'.repeat(Number(przebieg)), inline: false },
+        { name: 'OBSŁUGA KLIENTA:', value: '⭐️'.repeat(Number(obsluga)), inline: false },
       )
       .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
       .setFooter({ text: interaction.user.tag });
