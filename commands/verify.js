8d8ca1d13ab72ace0c0698ca30cae8dfdd4ecc2d
centraldm <michaltylec09@gmail.com>
2025-08-04 17:18:50 +0200

    Create verify.js

---
+++ commands/verify.js
@@ -0,0 +1,34 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('verify')
+    .setDescription('Wysyła wiadomość o backupie z linkiem Vaultcord'),
+
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setColor('#00acff')
+      .setTitle('✅ WYMIEŃ MAMONE × WERYFIKACJA')
+      .setDescription('**Zweryfikuj się**  aby nie stracić **naszego serwera.**')
+      .setImage('https://i.imgur.com/Eaz5OON.jpeg') // <-- Podmień na swój banner
+      .setTimestamp();
+
+    const button = new ButtonBuilder()
+      .setLabel('VAULTCORD')
+      .setStyle(ButtonStyle.Link)
+      .setURL('https://vaultcord.win/wymieńmamone')
+      .setEmoji('<:cord:1401945099159470190>');
+
+    const row = new ActionRowBuilder().addComponents(button);
+
+    await interaction.deferReply({ ephemeral: true });
+    await interaction.channel.send({ embeds: [embed], components: [row] });
+    await interaction.deleteReply();
+  },
+};
